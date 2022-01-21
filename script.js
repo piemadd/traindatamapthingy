ac65c5636bb3d0d92f0d3f54c94aa72ccd3cc813
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-01-21 06:48:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,154 @@
+const fetchRetry = ((url, delay, tries, fetchOptions = {}) => {
+    function onError(err){
+        triesLeft = tries - 1;
+        if(!triesLeft){
+            throw err;
+        }
+        return wait(delay).then(() => fetchRetry(url, delay, triesLeft, fetchOptions));
+    }
+    return fetch(url,fetchOptions).catch(onError);
+})
+
+let map = L.map('map', { zoomControl: false });
+
+const getAngle = (heading) => {
+    const angles = {
+        'NE': 0,
+        'E': 45,
+        'SE': 90,
+        'S': 135,
+        'SW': 180,
+        'SW': 225,
+        'W': 270,
+        'SW': 315
+    }
+    return angles[heading];
+}
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 25,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGllbWFkZCIsImEiOiJja2ltYzQ5YWMwcnZtMnJxbWttcGQ2MHF4In0.eHizY2ZVCp-mp0ViI5ZIEg'
+}).addTo(map);
+
+let icon = L.icon({
+    iconUrl: 'icon.png',
+    iconSize: [16, 16],
+    iconAnchor: [8, 8]
+});
+
+let pieroMarker = L.marker([50.5, 30.5], {icon: icon}).addTo(map);
+
+let markers = L.layerGroup().addTo(map);
+//let markers = []
+
+map.setView([39.14710270770074, -96.1962890625], 5);
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+(async () => {
+    let lineData = await fetchRetry('./nationalRoute.json', 100, 3);
+    let linesParsed = await lineData.json()
+
+    let features = linesParsed.features;
+
+    for (let i = 0; i < features.length; i++) {
+        let coordinates = features[i].geometry.coordinates;
+
+        let coordinatesUnscrambled = [];
+
+        for (let j = 0; j < coordinates.length; j++) {
+            coordinatesUnscrambled.push([coordinates[j][1], coordinates[j][0]])
+        }
+        
+        L.polyline(coordinatesUnscrambled, {color: 'red'}).addTo(map);
+    }
+    
+    let trainData = await fetchRetry('./listMin.json', 100, 3);
+    let trainsParsed = await trainData.json()
+
+    let keys = Object.keys(trainsParsed)
+
+    console.log(keys.length)
+
+    //for (let i = 0; i < keys.length; i++) {
+    //for (let i = 0; i < 10; i++) {
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    /*
+    let frame = urlParams.get('frame')
+    
+    if (frame == undefined) {
+        frame = 0
+    }
+
+    console.log(frame)
+
+    showInfo(frame, trainsParsed, keys)
+    */
+
+    let recordIt = urlParams.get('record')
+
+    console.log(recordIt)
+
+    if (recordIt == 'true') {
+        for (let i = 0; i < 10; i++) {
+        //for (let i = 0; i < 1590; i++) {
+            showInfo(i, trainsParsed, keys)
+            await delay(1000);
+        }
+    }
+    
+    
+    //}
+})();
+
+const showInfo = ((i, trainsParsed, keys) => {
+    let timeThingy = document.getElementById('time')
+    let tempList = trainsParsed[keys[i]]
+    for (let j = 0; j < tempList.length; j++) {
+        let marker = L.marker(tempList[j].coordinates, {rotationAngle: getAngle(tempList[j].heading), icon: icon});
+        //marker.addTo(map);
+        marker.addTo(markers)
+        //markers.push(marker);
+    }
+
+    let months = {
+        0: "January",
+        1: "February",
+        2: "March",
+        3: "April",
+        4: "May",
+        5: "June",
+        6: "July",
+        7: "August",
+        8: "September",
+        9: "October",
+        10: "November",
+        11: "December"
+    }
+
+    let date = new Date(parseInt(keys[i]))
+
+    console.log(keys[i])
+    
+    timeThingy.innerHTML = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} Central`
+    
+    console.log('next')
+
+    screenShot(`${i}.jpg`)
+})
+
+const screenShot = ((name = "image.jpg") => {
+    domtoimage.toJpeg(document.getElementsByTagName('body')[0], { quality: 0.95 })
+    .then(function (dataUrl) {
+        let link = document.createElement('a');
+        link.download = name;
+        link.href = dataUrl;
+        link.click();
+    });
+})
